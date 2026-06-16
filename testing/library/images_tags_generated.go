e10f86f7461c71b58aa870c069778559190eae9f
Alexandr Timofeev <alexandr.timofeev@flant.com>
2026-06-16 14:31:12 +0300

    [extended-monitoring] move module to external
    Signed-off-by: Alexandr Timofeev <alexandr.timofeev@flant.com>

--- testing/library/images_tags_generated.go
+++ testing/library/images_tags_generated.go
@@ -261,12 +261,6 @@ var DefaultImagesDigests = map[string]interface{}{
 		"docsBuilder": "imageHash-documentation-docsBuilder",
 		"web":         "imageHash-documentation-web",
 	},
-	"extendedMonitoring": map[string]interface{}{
-		"eventsExporter":             "imageHash-extendedMonitoring-eventsExporter",
-		"extendedMonitoringExporter": "imageHash-extendedMonitoring-extendedMonitoringExporter",
-		"imageAvailabilityExporter":  "imageHash-extendedMonitoring-imageAvailabilityExporter",
-		"x509CertificateExporter":    "imageHash-extendedMonitoring-x509CertificateExporter",
-	},
 	"ingressNginx": map[string]interface{}{
 		"controller112":         "imageHash-ingressNginx-controller112",
 		"controller114":         "imageHash-ingressNginx-controller114",
